fbdbd158a2051306ed813a432d8b666314728643
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-12 19:28:16 +0300

    Выносит код маленькой карточки фильма в отдельный компонент. Устанавливает пакет prop-types. Добавляет код для валидации всех props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Main from "../main/main.jsx";
-
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {genre, year} = props;
+// eslint-disable-next-line react/prop-types
+const App = ({genre, year, namesMovies}) => {
   return (
-    <Main genre={genre} year={year} />
+    <Main genre={genre} year={year} namesMovies={namesMovies} />
   );
 };
 
