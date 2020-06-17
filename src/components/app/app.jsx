d63a462c3ca597a0851f0d7a3ab007504ce54b12
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-17 17:35:23 +0300

    Установит и настроит jest

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Main from "../main/main.jsx";
 // eslint-disable-next-line react/prop-types
-const App = ({genre, year, namesMovies}) => {
+const App = ({genre, year, moviesNames}) => {
   return (
-    <Main genre={genre} year={year} namesMovies={namesMovies} />
+    <Main genre={genre} year={year} moviesNames={moviesNames} />
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const movieCard = {
   year: 2014
 };
 
-const namesMovies = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+const moviesNames = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
 
 ReactDOM.render(
-    <App genre={movieCard.genre} year={movieCard.year} namesMovies={namesMovies} />,
+    <App genre={movieCard.genre} year={movieCard.year} moviesNames={moviesNames} />,
     document.querySelector(`#root`)
 );
