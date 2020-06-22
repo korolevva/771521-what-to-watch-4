3655c65cfc84a0c633add534295ce7619c3a9067
VitaliyKorolev <korolevva88@yandex.ru>
2020-06-22 15:11:47 +0300

    Создает и описывает моки для карточки фильма

---
+++ src/mocks/films.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    src: `img/bohemian-rhapsody.jpg`,
+    title: `Bohemian Rhapsody`,
+  }, {
+    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  }, {
+    src: `img/macbeth.jpg`,
+    title: `Macbeth`,
+  }, {
+    src: `img/aviator.jpg`,
+    title: `Aviator`,
+  }, {
+    src: `img/we-need-to-talk-about-kevin.jpg`,
+    title: `We need to talk about Kevin`,
+  }, {
+    src: `img/what-we-do-in-the-shadows.jpg`,
+    title: `What We Do in the Shadows`,
+  }, {
+    src: `img/revenant.jpg`,
+    title: `Revenant`,
+  }, {
+    src: `img/johnny-english.jpg`,
+    title: `Johnny English`,
+  },
+];
